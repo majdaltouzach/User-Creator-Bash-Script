2163aa19df995c70a1c9e61bd269f555ee9f1234
majdaltouzach <majdalanitoufic@gmail.com>
2022-04-26 17:30:50 -0700

    modified:   Proj1_Toufic-Fixed.sh 	modified:   ../../templates/CS125_Proj1_template.sh

--- proj/src/Proj1_Toufic-Fixed.sh
+++ proj/src/Proj1_Toufic-Fixed.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
+###
+ #perform PROJECT1 for CS125: Create user account and assign password from words.txt.
+ #Coded by: Toufic Majdalani
+ ###
+
+
+ read -p
+
+ cd ~
+
+mkdir ~/sandbox
+
 
