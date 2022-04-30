a3962630f35aea8cfedcd4d1317eddc4dff3e787
majdaltouzach <majdalanitoufic@gmail.com>
2022-04-29 18:55:59 -0700

    Finished Script Release

--- proj/src/My_Project1.sh
+++ proj/src/My_Project1.sh
@@ -2,48 +2,72 @@
 
 ###
 #perform PROJECT1 for CS125: Create user account and assign password from words.txt.
-#Coded by: Toufic Majdalani
+#Coded by: <your name>
 ###
 
 read -p "Please run this script using sudo, else will not assign password to users and cannot copy shadow file to Sandbox. Press Enter key to proceed."
 
-#cd /home/linux-class/
-
 ##
 #Create sandbox directory in home
 mkdir sandbox2
-
-cd sandbox2
 ##
+cd sandbox2
 
 ##
 #Download password file online
 ##
-wget https://raw.githubusercontent.com/danielmiessler/SecLists/master/Passwords/twitter-banned.txt -O passwordlist.txt
+wget https://bit.ly/3F2W0YL -O passwordlist.txt
 
 
-##From passwordlist.txt, shuffle and randomly select 100 entries then save it to words.txt in the sandbox directory
+##From passwordlist.txt, shuffle and randomly select 100 entries then save it to passwordlist.txt in the sandbox directory
 
-#cd to the sandbox
-#cd sandbox
+
+
 
 ##
 #create5  users, generate and assign password from words.txt 
 ##
+
 for i in {1..5}
 do
-	randompw=$(shuf words.txt | tail -n 1)
-	useradd -m user1
-	echo user1:$randompw | chpasswd
-	echo "UserID:" user1 "has been created with the following password:" 	    $randompw
+	randompw=$(shuf passwordlist.txt | tail -n 1)
+	useradd -m user$i
+	echo user$i:$randompw | chpasswd
+	echo "UserID": user$i "has been created with the following password:" $randompw
 
 done
 
 
+#randompw=$(shuf passwordlist.txt | tail -n 1)
+#useradd -m user1
+#echo user1:$randompw | chpasswd
+#echo "UserID:" user1 "has been created with the following password:" $randompw
+
+#randompw=$(shuf passwordlist.txt | tail -n 1)
+#useradd -m user2
+#echo user1:$randompw | chpasswd
+#echo "UserID:" user1 "has been created with the following password:" $randompw
+
+#randompw=$(shuf passwordlist.txt | tail -n 1)
+#useradd -m user3
+#echo user1:$randompw | chpasswd
+#echo "UserID:" user1 "has been created with the following password:" $randompw
+
+#randompw=$(shuf passwordlist.txt | tail -n 1)
+#useradd -m user4
+#echo user1:$randompw | chpasswd
+#echo "UserID:" user1 "has been created with the following password:" $randompw
+
+#randompw=$(shuf passwordlist.txt | tail -n 1)
+#useradd -m user5
+#echo user1:$randompw | chpasswd
+#echo "UserID:" user1 "has been created with the following password:" $randompw
 
 
 
 ##
 #copy shadow file from /etc/shadow to sandbox directory
 ##
-cp -ar /etc/shadow sandbox2
+cp -ar /etc/shadow ./
+echo Copying /etc/shadow to /home/$UserID/sandbox2
+echo done
